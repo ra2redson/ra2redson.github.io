c282acab8f666b56d2493b2feddb2f7b96b35e28
ra2redson <ra2redson@users.noreply.github.com>
2025-03-11 15:22:47 +0000

    deploy: a079eda32c25312c274097d882ff0df1d5410dd8

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -20,3 +20,10 @@ document.addEventListener("touchstart", function (event) {
         }
     } // <-- Dấu } bị thiếu ở đây
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('a[href^="http"]').forEach(link => {
+        link.setAttribute("target", "_blank");
+        link.setAttribute("rel", "noopener noreferrer");
+    });
+});
